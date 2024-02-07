80bb906ea0befeb7208fb1c321246d87845f2373
Ayush Verma <42459801+vermaayush680@users.noreply.github.com>
2024-02-07 23:48:18 +0530

    Update UserWidget.jsx

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -23,7 +23,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await axios(`https://app-5829c58f-c705-4acc-9979-3de5891c6144.cleverapps.io/user/${userId}`, {
+    const response = await axios(`https://sociogeeks-backend.onrender.com/user/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
